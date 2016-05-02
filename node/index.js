2d637effc5a1e427ff63b852c71c4359bc7a14d4
jaypoon <panjh1986@126.com>
2016-05-03 00:52:29 +0800

    update

--- node/index.js
+++ node/index.js
@@ -29,3 +29,6 @@ app.get('/', function(req, res, next) {
 http.createServer(app).listen(process.env.PORT || 8080, function() {
   console.log('Listening on port ' + (process.env.PORT || 8080));
 });
+
+
+console.log("Finish")
